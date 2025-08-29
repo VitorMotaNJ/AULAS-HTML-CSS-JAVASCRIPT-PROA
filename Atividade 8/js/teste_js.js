55b970620d6b5d44d35689e96249db0f9a1945f0
vitor-mota <vitormotanovais@gmail.com>
2025-08-29 12:07:45 -0300

    Aplicando funcionalidade javascript

--- Atividade 8/js/teste_js.js
+++ Atividade 8/js/teste_js.js
@@ -1,3 +1,23 @@
-function exibe_ferrari(){
-    
+function exibe_camaro(){
+    document.getElementById("camaro-amarelo").style.visibility="visible";
+    document.getElementById("fusca-azul").style.visibility="hidden";
+    document.getElementById("uno").style.visibility="hidden";
+}
+
+function exibe_fusca(){
+    document.getElementById("fusca-azul").style.visibility="visible"
+    document.getElementById("uno").style.visibility="hidden";
+    document.getElementById("camaro-amarelo").style.visibility="hidden";
+}
+
+function exibe_uno(){
+    document.getElementById("fusca-azul").style.visibility="hidden"
+    document.getElementById("uno").style.visibility="visible";
+    document.getElementById("camaro-amarelo").style.visibility="hidden";
+}
+
+function sumir_carro(){
+    document.getElementById("fusca-azul").style.visibility="hidden"
+    document.getElementById("uno").style.visibility="hidden";
+    document.getElementById("camaro-amarelo").style.visibility="hidden";
 }
